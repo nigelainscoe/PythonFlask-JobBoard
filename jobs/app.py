cb9e171fa3dc17a6ad1d550fddbef37ef0a91a8f
Nigel Ainscoe <nigel.ainscoe@parkwharf.com>
2019-10-28 22:26:46 +0000

    Display All Jobs

--- jobs/app.py
+++ jobs/app.py
@@ -2,8 +2,7 @@ from flask import Flask, g
 from flask import render_template
 import sqlite3
 
-PATH='db/jobs.sqlite'
-
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
@@ -14,6 +13,7 @@ def open_connection():
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
+
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
@@ -26,13 +26,18 @@ def execute_sql(sql, values=(), commit=False, single=False):
     cursor.close()
     return results
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
 
+
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql(
+        'SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id'
+    )
+    return render_template('index.html', jobs=jobs)
