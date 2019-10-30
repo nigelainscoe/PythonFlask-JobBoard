6dd63ebc9a275fdf16bc5093975ad6e44e25c264
Nigel Ainscoe <nigel.ainscoe@parkwharf.com>
2019-10-30 22:44:40 +0000

    Fix index error (missing return)

--- jobs/app.py
+++ jobs/app.py
@@ -59,8 +59,10 @@ def employer(employer_id):
     jobs = execute_sql(
         'SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?',
         [employer_id])
-    jobs = execute_sql(
+    reviews = execute_sql(
         'SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?',
-        [employer_id]
-    )
-    render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
+        [employer_id])
+    return render_template('employer.html',
+                           employer=employer,
+                           jobs=jobs,
+                           reviews=reviews)
